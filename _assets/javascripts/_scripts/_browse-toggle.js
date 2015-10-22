af6791ebb229adbe317600404023e9bc45b43a77
Jared Cunha <jaredcunha@gmail.com>
2015-10-21 20:43:39 -0400

    add addition list/map functionality

---
+++ _assets/javascripts/_scripts/_browse-toggle.js
@@ -0,0 +1,29 @@
+var inventoryToggleList = $('#inventoryToggleList');
+var inventoryToggleMap = $('#inventoryToggleMap');
+var inventoryViewToggleItem = $('.inventory-view-toggle__item');
+var browseList = $('#browseList');
+var browseMap = $('#browseMap');
+
+clearToggleSelection = function() {
+	inventoryViewToggleItem.removeClass("active");
+};
+
+
+
+$(inventoryToggleList).on('click', function(){
+	clearToggleSelection();
+	$(this).addClass("active");
+	browseList.addClass("in-view");
+	browseMap.removeClass("in-view");
+});
+
+$(inventoryToggleMap).on('click', function(){
+	clearToggleSelection();
+	$(this).addClass("active");
+	browseMap.addClass("in-view");
+	browseList.removeClass("in-view");
+});
+
+
+
+
--- assets/dev/js/global.js
+++ assets/dev/js/global.js
@@ -10350,6 +10350,36 @@ return jQuery;
 
 }));
 
+var inventoryToggleList = $('#inventoryToggleList');
+var inventoryToggleMap = $('#inventoryToggleMap');
+var inventoryViewToggleItem = $('.inventory-view-toggle__item');
+var browseList = $('#browseList');
+var browseMap = $('#browseMap');
+
+clearToggleSelection = function() {
+	inventoryViewToggleItem.removeClass("active");
+};
+
+
+
+$(inventoryToggleList).on('click', function(){
+	clearToggleSelection();
+	$(this).addClass("active");
+	browseList.addClass("in-view");
+	browseMap.removeClass("in-view");
+});
+
+$(inventoryToggleMap).on('click', function(){
+	clearToggleSelection();
+	$(this).addClass("active");
+	browseMap.addClass("in-view");
+	browseList.removeClass("in-view");
+});
+
+
+
+
+
 toggleOverlay = function() {
 	var overlay = $('#overlay');
 	if ( $(overlay).length ) {
